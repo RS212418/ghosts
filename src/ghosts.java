44535355bc0d188c8f436ae3372a0f3f8641e0b8
RS212418 <roman.r.siva@gmail.com>
2022-02-11 10:49:53 +0000

    Initial commit

---
+++ src/ghosts.java
@@ -0,0 +1,15 @@
+public class ghosts {
+
+    protected String colour;
+    protected int Speed;
+    protected String hairColour;
+
+
+
+
+    public ghosts(String colour, int speed, String hairColour){
+        
+    }
+
+
+}
